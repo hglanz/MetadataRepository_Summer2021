822cecc02fbcda67f8961c0c7f62cff655ae972f
kingsuching <sucheen.sundaram@gmail.com>
2021-06-21 13:06:41 -0700

    Wrote code to scrape description for a dataset from datahub.io

--- scrape_rvest.R
+++ scrape_rvest.R
@@ -3,7 +3,14 @@
 #' @import stringr
 #' @import dplyr
 #' @import magrittr
+#' @param url the website url
+#' @param type the html formatting type to extract the correct items
+#' @export scrape_rvest
 
-scrape_rvest <- function(url) {
-  h <- rvest::read_html(url)
+scrape_rvest <- function(url, type) {
+  html_fm <- rvest::read_html(url)
+  descriptions <- html_fm %>% rvest::html_nodes(type) %>% html_text() %>%
+    str_remove_all("\n") %>%
+    str_trim()
+  return(descriptions)
 }
